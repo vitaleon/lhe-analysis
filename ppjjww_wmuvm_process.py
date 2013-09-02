e411d74564c79348241763cb05d759219757e855
krotny <krotny@gmail.com>
2013-09-02 18:22:10 +0200

    More general and external calculation of kinematic variables.

--- ppjjww_wmuvm_process.py
+++ ppjjww_wmuvm_process.py
@@ -7,6 +7,8 @@ import util
 from parsing import eventlines_to_particlesdict
 from loading import LHELoader
 from math import *
+from variables import *
+
 
 def name_particles(particles):
     """Adds to particles field 'name' that is constructed as PDG_orderno. 
@@ -54,123 +56,43 @@ def name_particles(particles):
     return particles   
 
 
-
-
-
-
 def calculate_dependent_variables(particles):
-
-    #################################################################
+    """Basing on parameteres of particles calculates set of kinematic variables."""
     j1 = particles["j_1"]
     j2 = particles["j_2"]
-    w1 = particles["w_1"]
-    w2 = particles["w_2"]
     l1 = particles["l_1"]
     l2 = particles["l_2"]
-    #################################################################
+    particles = {"j1": j1, "j2": j2, "l1":l1, "l2":l2}
 
-    #TRANSVERSAL MOMENTUM ###########################################
-    #ptj1=sqrt(p(1,i)**2+p(2,i)**2)
-    ptj1 = sqrt( j1["p1"]**2 + j1["p2"]**2 )
-    ptj2 = sqrt( j2["p1"]**2 + j2["p2"]**2 )
-
-    ptw1 = sqrt( w1["p1"]**2 + w1["p2"]**2 )
-    ptw2 = sqrt( w2["p1"]**2 + w2["p2"]**2 )
-
-    ptl1 = sqrt( l1["p1"]**2 + l1["p2"]**2 )
-    ptl2 = sqrt( l2["p1"]**2 + l2["p2"]**2 )
-
-    #TOTAL MOMENTUM #################################################
-    #pj1=sqrt(p(1,i)**2+p(2,i)**2+p(3,i)**2)
-    pj1 = sqrt( j1["p1"]**2 + j1["p2"]**2 + j1["p3"]**2  )
-    pj2 = sqrt( j2["p1"]**2 + j2["p2"]**2 + j2["p3"]**2  )
-    pl1 = sqrt( l1["p1"]**2 + l1["p2"]**2 + l1["p3"]**2  )
-    pl2 = sqrt( l2["p1"]**2 + l2["p2"]**2 + l2["p3"]**2  )
-
-    #ANGLE: PHI #####################################################
-    phi_l1 = atan2(l1["p2"], l1["p1"])
-    phi_l2 = atan2(l2["p2"], l2["p1"])    
-    delta_phi_ll = min(abs(phi_l1-phi_l2), 2*pi - abs(phi_l2-phi_l1))
-
-    #INVARIANT MASES ################################################ 
-    M_jj   = sqrt( -(j1["p1"]+j2["p1"])**2 -(j1["p2"]+j2["p2"])**2 -(j1["p3"]+j2["p3"])**2 +(j1["p4"]+j2["p4"])**2 )
-    M_j1l1 = sqrt( -(j1["p1"]+l1["p1"])**2 -(j1["p2"]+l1["p2"])**2 -(j1["p3"]+l1["p3"])**2 +(j1["p4"]+l1["p4"])**2 ) 
-    M_j2l2 = sqrt( -(j2["p1"]+l2["p1"])**2 -(j2["p2"]+l2["p2"])**2 -(j2["p3"]+l2["p3"])**2 +(j2["p4"]+l2["p4"])**2 ) 
-    M_j1l2 = sqrt( -(j1["p1"]+l2["p1"])**2 -(j1["p2"]+l2["p2"])**2 -(j1["p3"]+l2["p3"])**2 +(j1["p4"]+l2["p4"])**2 ) 
-    M_j2l1 = sqrt( -(l1["p1"]+j2["p1"])**2 -(l1["p2"]+j2["p2"])**2 -(l1["p3"]+j2["p3"])**2 +(l1["p4"]+j2["p4"])**2 )  
-
-    #FUNCTIONS ######################################################
-    #R_pT = (pT_l1 pT_l2) / (pT_j1 pT_j2). ?
-    R_pT = float(ptl1 * ptl2) / float(ptj1 * ptj2)
-
-
-    #################################################################
-
-    #cosa=p(3,i)/pj1
-    #sina=ptj1/pj1
-    #etaj1 = -log((1.-cosaj1)/sinaj1) 
-    cosaj1 = (j1["p3"])/pj1 #abs() for MadAnalysis compatibility
-    sinaj1 = ptj1/pj1
-    etaj1 = -log( (1.0-cosaj1) / sinaj1 )
-    #etaj1 = 0.5 * log( float(pj1+j1["p3"])/float(pj1-j1["p3"]) ) 
-
-       
-    cosaj2 = (j2["p3"])/pj2 #abs() for MadAnalysis compatibility
-    sinaj2 = ptj2/pj2
-    etaj2 = -log((1.0-cosaj2)/sinaj2)
-    #etaj2 = 0.5 * log( float(pj2+j2["p3"])/float(pj2-j2["p3"]) ) 
-
-    cosal1 = (l1["p3"])/pl1 #abs() for MadAnalysis compatibility
-    sinal1 = ptl1/pl1
-    etal1 = -log( (1.0-cosal1) / sinal1 )
-
-    cosal2 = (l2["p3"])/pl2 #abs() for MadAnalysis compatibility
-    sinal2 = ptl2/pl2
-    etal2 = -log( (1.0-cosal2) / sinal2 )
-
-
-    #################################################################
-    variables = {"ptl1":ptl1, "ptl2":ptl2, "M_j1l1":M_j1l1, "M_j2l2":M_j2l2,
-                 "ptj1":ptj1, "ptj2":ptj2, "pj1":pj1, "pj2":pj2, 
-                 "cosaj1":cosaj1, "cosaj2":cosaj2,
-                 "sinaj1":sinaj1, "sinaj2":sinaj2, 
-                 "etaj1":etaj1, "etaj2":etaj2, "etal1":etal1, "etal2":etal2,  
-                 "R_pT": R_pT, "delta_phi_ll":delta_phi_ll,
-                 "M_jj":M_jj, "M_j1l2":M_j1l2, "M_j2l1":M_j2l1 }    
+    variables = {}
+    update_variables_by_single(variables, particles, momentum, "p")
+    update_variables_by_single(variables, particles, pT, "pt")
+    update_variables_by_single(variables, particles, eta, "eta")
+    update_variables_by_single(variables, particles, eta_ma, "etama")
+    update_variables_by_unordered_pair(variables, particles, invariant_mass, "M_")
+    update_variables_by_unordered_pair(variables, particles, delta_phi, "delta_phi_")
+    variables["R_pT"] = R_pT_coeff(l1,l2,j1,j2)
+    
+    #print variables
     return variables
 
 
 
 def is_event_valid(particles, variables):
-    """Should the event be kept?"""
+    """Should the event be kept or not?"""
     p, v = particles, variables #short abbreviations
 
-    #print_list( lines, "lines")
-    #print_list( particles_list, "particles_list" )
-    #print_dict( particles, "particles" )
-    #print_dict( variables, "variables" )
-    #sys.exit(-1)
-    #return True
-    #print variables["etaj1"]
     #return variables["etaj1"]>4.0 and variables["etaj1"]<5.0 # and variables["etaj2"]>4.0 and variables["etaj2"]<5.0
     #return variables["ptj1"]>=10.0 and variables["ptj2"]>=7.0 and variables["ptl1"]>=11.0 and variables["ptl2"]>=11.0
     #return variables["M_j2l2"]>=200
     #return variables["ptl2"] >= 14.0
-    return variables["etaj1"] >= 3
+    #return variables["etaj1"] >= 3
 
-    #variables = {"ptl1":ptl1, "ptl2":ptl2, "M_j1l1":M_j1l1, "M_j2l2":M_j2l2,
-    #            "ptj1":ptj1, "ptj2":ptj2, "pj1":pj1, "pj2":pj2, 
-    #             "cosaj1":cosaj1, "cosaj2":cosaj2,
-    ##             "sinaj1":sinaj1, "sinaj2":sinaj2, 
-    #             "etaj1":etaj1, "etaj2":etaj2, "etal1":etal1, "etal2":etal2,  
-    #             "R_pT": R_pT, "delta_phi_ll":delta_phi_ll,
-    #             "M_jj":M_jj, "M_j1l2":M_j1l2, "M_j2l1":M_j2l1 } 
-
-    #return 2<abs(v["etaj1"]) and abs(v["etaj1"])<5 and \
-    #       2<abs(v["etaj2"]) and abs(v["etaj2"])<5 and \
-    #       v["M_j1l2"]>200 and v["M_j2l1"]>200 and \
-    #       v["M_jj"]>500 and v["R_pT"]>3.5 and v["delta_phi_ll"]>2.5 \
-    #        and abs(v["etaj1"]-v["etaj2"]) > 4
+    return 2<abs(v["etaj1"]) and abs(v["etaj1"])<5 and \
+           2<abs(v["etaj2"]) and abs(v["etaj2"])<5 and \
+           v["M_j1l2"]>200 and v["M_j2l1"]>200 and \
+           v["M_j1j2"]>500 and v["R_pT"]>3.5 and v["delta_phi_l1l2"]>2.5 \
+            and abs(v["etaj1"]-v["etaj2"]) > 4
 
 
 if __name__=="__main__":
---
+++ variables.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Particles' kinematic variables calculation."""
+from math import *
+import itertools
+
+def invariant_mass(j1,j2):
+    M_jj = sqrt( -(j1["p1"]+j2["p1"])**2 -(j1["p2"]+j2["p2"])**2 -(j1["p3"]+j2["p3"])**2 +(j1["p4"]+j2["p4"])**2 )
+    return M_jj
+
+def delta_phi(l1,l2):
+    phi_l1 = atan2(l1["p2"], l1["p1"])
+    phi_l2 = atan2(l2["p2"], l2["p1"])    
+    delta_phi_ll = min(abs(phi_l1-phi_l2), 2*pi - abs(phi_l2-phi_l1))
+    return delta_phi_ll
+
+def momentum(j1):
+    """Total momentum: p=sqrt(p(1,i)**2+p(2,i)**2+p(3,i)**2)."""
+    return sqrt( j1["p1"]**2 + j1["p2"]**2 + j1["p3"]**2  )
+
+def pT(j1):
+    """Transversal momentum: pT=sqrt(p(1,i)**2+p(2,i)**2)."""
+    ptj1 = sqrt( j1["p1"]**2 + j1["p2"]**2 )
+    return ptj1
+
+def eta(j1):
+    """etaj1 = -log((1.-cosa)/sina) where cosa=p(3,i)/pj1, sina=ptj1/pj1 """
+    pj1 = momentum(j1)
+    ptj1 = pT(j1)
+
+    cosaj1 = (j1["p3"])/pj1 
+    sinaj1 = ptj1/pj1
+    etaj1 = -log( (1.0-cosaj1) / sinaj1 )
+    return etaj1
+
+def eta_ma(j1):
+    pj1 = momentum(j1)
+    ptj1 = pT(j1)
+
+    cosaj1 = abs(j1["p3"])/pj1 #abs() for MadAnalysis compatibility
+    sinaj1 = ptj1/pj1
+    etaj1 = -log( (1.0-cosaj1) / sinaj1 )
+    return etaj1
+
+
+def R_pT_coeff(l1,l2,j1,j2):
+    """R_pT = (pT_l1 pT_l2) / (pT_j1 pT_j2)"""
+    R_pT = float(pT(l1) * pT(l2)) / float(pT(j1) * pT(j2))
+    return R_pT
+
+
+######################################################################################
+
+
+def update_variables_by_single(variables, particles, variable_func, variable_prefix, sep=""):
+    for particle_name, particle in particles.iteritems():
+        variable_name = variable_prefix+sep+particle_name
+        variables[variable_name] = variable_func(particle)
+
+def update_variables_by_unordered_pair(variables, particles, variable_func, variable_prefix, sep=""):
+    particles_list = sorted(list(particles.iteritems()))
+    for (p1_name,p1), (p2_name,p2) in itertools.combinations(particles_list, 2):
+        variable_name = variable_prefix+sep+p1_name+sep+p2_name
+        variables[variable_name] = variable_func(p1, p2)
+        
+
