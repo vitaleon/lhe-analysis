c36e2b3dc4da987cdc35f443e2a348e0a9a02bef
krotny <krotny@gmail.com>
2013-10-02 13:48:46 +0200

    R code removal.

--- ppjjww_wmuvm_compare.r
+++
@@ -1,6 +0,0 @@
-
-path1 = "../../data/ppjjww_muvm_h1e10_genok.csv"
-path2 = "../../data/ppjjww_muvm_h126c10_genok.csv"
-
-d1 = read.table(path1, header = TRUE, sep =",")
-d2 = read.table(path2, header = TRUE, sep =",")
